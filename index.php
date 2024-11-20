889688177fe73f493f7162deed93d28a8d561f25
Future Computer Training Institute <107338637+fctiadmin@users.noreply.github.com>
2024-11-20 11:52:39 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,45 @@
+<?php 
+//*********Logic *'''''''/
+// $bangla=70;
+// $math=60;
+// $english=75;
+// $total_mark=$bangla+$math+$english;
+// if($bangla<33 || $math<33 || $english<33){
+//     echo "Your Grade is F";
+// }
+// elseif($total_mark>=240){
+//     echo "Your Grade is A+";
+// }
+// elseif($total_mark>=210){
+//     echo "Your Grade is A";
+// }
+// elseif($total_mark>=180){
+//     echo "Your Grade is A-";
+// }
+// elseif($total_mark>=150){
+//     echo "Your Grade is B";
+// }
+// elseif($total_mark>=120){
+//     echo "Your Grade is C";
+// }
+// elseif($total_mark>=99){
+//     echo "Your Grade is D";
+// }
+
+//********* */ simple function***************** 
+
+// function add($x,$y){
+//     $result=$x+$y;
+//     return $result;
+// }
+// echo add(20,30);
+
+
+// Loop section
+$sl=1;
+while($sl<=100){
+    echo $sl."<br>";
+    $sl++;
+}
+
+?>
